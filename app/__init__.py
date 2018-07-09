3c7401ba2f30cffb52e56b409edc318f5cc5c629
tuananh <anh.nt094@gmail.com>
2018-07-08 23:00:40 -0700

    add bootstrap to jobs_per_page.html

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from config import Config
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -10,6 +11,9 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+
+Bootstrap(app)
+
 
 # Khai bao trang login - login() function, se dung url_for() de tim duong dan
 login.login_view = 'login'  
